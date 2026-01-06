401fc3b46f2b6ae7157fb23c2e27b4c3f971693f
sergio <sergio@vbox.myguest.virtualbox.org>
2026-01-06 22:42:00 +0100

    feat(end-project): final project

--- C/PracticaTema6/programa1.c
+++
@@ -1,6 +0,0 @@
-
-#include <stdio.h>
-
-int main() {
-    printf("Este es el programa n1");
-}
--- C/SistemasOperativos/PracticaTema6/programa2.c
+++ C/SistemasOperativos/PracticaTema6/programa2.c
@@ -1,6 +1,52 @@
-
 #include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
 
-int main(){
-    printf("Este es el programa n2");
+int main() {
+    int pid;
+    int option;
+
+    printf("Introduzca el PID del proceso receptor: \n");
+    scanf("%d", &pid);
+
+    // Menú de opciones
+    printf("¿Qué desea hacer?\n");
+    printf("1. Pausar el proceso\n");
+    printf("2. Reanudar el proceso\n");
+    printf("3. Salir (matar proceso)\n");
+    scanf("%d", &option);
+
+    // Procesar la opción seleccionada
+    switch(option) {
+        case 1:
+            // Enviar señal SIGUSR1 para pausar el proceso
+            if (kill(pid, SIGUSR1) == 0) {
+                printf("El proceso con PID %d ha sido pausado.\n", pid);
+            } else {
+                perror("Ha habido un error al pausar el proceso");
+            }
+            break;
+
+        case 2:
+            // Enviar señal SIGUSR2 (id=12) para reanudar el proceso
+            if (kill(pid, 12) == 0) {
+                printf("El proceso con PID %d ha sido reanudado.\n", pid);
+            } else {
+                perror("Ha habido un error al reanudar el proceso");
+            }
+            break;
+
+        case 3:
+            if(kill(pid, SIGTERM) == 0){
+                printf("Proceso %d killed, saliendo...\n", pid);
+            }
+            exit(0);
+            break;
+
+        default:
+            printf("Opción no válida. Saliendo...\n");
+            exit(1);
+    }
+
+    return 0;
 }
